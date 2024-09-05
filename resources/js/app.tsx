4c50b85d0217377846f288f04b39fc29a78bd082
Santu Roy <santuxroy@gmail.com>
2024-09-06 00:29:20 +0600

    regular react setup with tsx support

---
+++ resources/js/app.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+// import AllRoutes from './routes';
+
+ReactDOM.createRoot(document.getElementById('app')!).render(
+	<React.StrictMode>
+        <>hello</>
+    </React.StrictMode>
+)
